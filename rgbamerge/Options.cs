185d82c0a6a194e95dc230354f42ec47b6b81021
Vladimir Bodurov <vladimir@bodurov.com>
2020-09-23 18:51:49 -0700

    readme changed

--- rgbamerge/Options.cs
+++ rgbamerge/Options.cs
@@ -4,15 +4,15 @@ namespace rgbamerge
 {
     public class Options
     {
-        [Option('r', "red", Required = false, HelpText = "Red channel image")]
+        [Option('r', "red", Required = false, HelpText = "Red channel image or color as byte number 0-255")]
         public string Red { get; set; }
-        [Option('g', "green", Required = false, HelpText = "Green channel image")]
+        [Option('g', "green", Required = false, HelpText = "Green channel image or color as byte number 0-255")]
         public string Green { get; set; }
-        [Option('b', "blue", Required = false, HelpText = "Blue channel image")]
+        [Option('b', "blue", Required = false, HelpText = "Blue channel image or color as byte number 0-255")]
         public string Blue { get; set; }
-        [Option('a', "alpha", Required = false, HelpText = "Alpha channel image")]
+        [Option('a', "alpha", Required = false, HelpText = "Alpha channel image or color as byte number 0-255")]
         public string Alpha { get; set; }
-        [Option('o', "output", Required = true, HelpText = "Output image")]
+        [Option('o', "output", Required = true, HelpText = "Output image path")]
         public string Output { get; set; }
     }
 }
